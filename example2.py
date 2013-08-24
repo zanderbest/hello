801d983952f398a0027dbd7ae64b476fad39749e
zanderbest <zanderbest@gmail.com>
2013-08-24 18:24:45 -0500

    Test checking what is commited with unstaged files and commit -a Same file, changed, saved, added, changed again, then commit -a without staging/adding.

---
+++ example2.py
@@ -0,0 +1,12 @@
+# A comment
+#YAAYH THIS GETS IGNORED
+
+print "words" #but these words are invsible
+
+#print "this wont word"
+
+print "But this will word"
+
+#yay
+
+print "This file was created to test staged/unstaged states with git commits"
