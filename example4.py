a5a44b7ca3c739b30155ff06cf5f9794fa60822c
zanderbest <zanderbest@gmail.com>
2013-08-26 18:26:58 -0500

    fix cars_driven var not defined

--- example4.py
+++ example4.py
@@ -2,6 +2,7 @@ cars = 100
 space_in_a_car = 4.0
 drivers = 30
 passengers = 90
+cars_driven = drivers
 cars_not_driven = cars - drivers
 carpool_capacity = cars_driven * space_in_a_car
 avg_passengers_per_car = passengers / cars_driven
@@ -12,3 +13,4 @@ print "There wioll be", cars_not_driven, "empty cars today."
 print "We can Transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpool today."
 print "We need to put about", avg_passengers_per_car, "in each car."
+print "Hey %s there." % "you"
