58d72ec26fc4005b3ca17ca7e7f437ee993b777d
zanderbest <zanderbest@gmail.com>
2013-08-26 18:39:38 -0500

    remove my_ prefix from variables for better reading

--- ex5.py
+++ ex5.py
@@ -1,14 +1,17 @@
-my_name = 'Zander Best' 	#Defining name
-my_age = 24					#Defining age in years
-my_height = 74				#defining height in inches
-my_weight = 230				#defining weight in US pounds
-my_eyes = 'blue'			#Defining eye color
-my_teeth = 'white'			#defining teeth color? I was told to
-my_hair = 'light brown'		#defining my hair color
+name = 'Zander Best' 	#Defining name
+age = 24					#Defining age in years
+height = 74				#defining height in inches
+weight = 230				#defining weight in US pounds
+eyes = 'blue'			#Defining eye color
+teeth = 'white'			#defining teeth color? I was told to
+hair = 'light brown'		#defining my hair color
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_weight + my_height)
+#using %s to add a string
+print "Let's talk about %s." % name
+
+#using %d to add a ...decimal? digit?
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + weight + height)
