47980d099af62ca198333094e0c6f2d553e8f060
zanderbest <zanderbest@gmail.com>
2013-08-26 18:58:01 -0500

    add exercise 5 through 10

--- ex5.py
+++ ex5.py
@@ -15,3 +15,19 @@ print "He's %d pounds heavy." % weight
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + weight + height)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6 + end7,
+print end8 + end9 + end10 + end11 + end12
