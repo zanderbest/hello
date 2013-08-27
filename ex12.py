a8a3226e363fc4ec43dc3d40ec8b46d60d67a304
zanderbest <zanderbest@gmail.com>
2013-08-26 19:23:59 -0500

    add initial commit for exercise 12

---
+++ ex12.py
@@ -0,0 +1,12 @@
+#Exercise 12 of Learn Python 12 the Hardway
+#Exercise done by Zander Best
+#Learn Python the Hard way is Copyright (C) 2010 Zed. A. Shaw
+#
+#I'm concurrently learning Git along with Zed's Guide. 
+
+#Giving raw_input a prompt
+name = raw_input("Name? ")			#Final space is intentional
+
+#nto sure what the difference is between these two
+print "Your name is %s" % name
+print "Your name is", name
