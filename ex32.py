1f855004013987e199fa2e7af9fb850c3fcb5a66
zanderbest <zanderbest@gmail.com>
2013-08-27 00:37:47 -0500

    add exercise 32 init commit

---
+++ ex32.py
@@ -0,0 +1,8 @@
+#Exercise 32 of Learn Python the Hardway
+#Exercise done by Zander Best
+#Learn Python the Hard way is Copyright (C) 2010 Zed. A. Shaw
+#
+#I'm concurrently learning Git along with Zed's Guide. 
+
+hairs = ['brown', 'blond', 'red']
+print hairs
