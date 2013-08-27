971133d13ecf02003eef51aa09101cda2edcf7de
zanderbest <zanderbest@gmail.com>
2013-08-26 19:00:01 -0500

    fix new line per question

--- ex11.py
+++ ex11.py
@@ -1,13 +1,13 @@
 #ask a question, wait for response
-print "How old are you?"
+print "How old are you?",
 age = raw_input()
 
 #ask a question, wait for response
-print "How tall are you in inches"	
+print "How tall are you in inches",
 height = raw_input()
 
 #ask a question, wait for response
-print "How much do you weigh in pounds"
+print "How much do you weigh in pounds",
 weight = raw_input()
 
 print "You are %s years old, %s inches tall, and %s lbs heavy" % (age, height, weight)
