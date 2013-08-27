aefa235a24196082a5429cc2912c8b113d02df90
zanderbest <zanderbest@gmail.com>
2013-08-27 00:04:09 -0500

    add exercise 29 init commit

---
+++ ex29.py
@@ -0,0 +1,12 @@
+#Exercise 29 of Learn Python the Hardway
+#Exercise done by Zander Best
+#Learn Python the Hard way is Copyright (C) 2010 Zed. A. Shaw
+#
+#I'm concurrently learning Git along with Zed's Guide. 
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats! The World is doomed!"
