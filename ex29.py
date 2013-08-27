80c0dc10816f7f89d1225353784c1e7fa97803e2
zanderbest <zanderbest@gmail.com>
2013-08-27 00:06:27 -0500

    add many more ifs

--- ex29.py
+++ ex29.py
@@ -10,3 +10,23 @@ dogs = 15
 
 if people < cats:
 	print "Too many cats! The World is doomed!"
+
+if people > cats:
+	print "Not many cats! The world is saved!"
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people > dogs:
+	print "The World is dry"
+
+dogs += %
+
+if people >= dogs:
+	print "People are greater or equal to dogs"
+
+if people <= dogs:
+	print "People are less than or equal to dogs"
+
+if people == dogs
+	print "People are dogs"
