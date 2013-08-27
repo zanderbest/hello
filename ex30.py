f6af9b2e61134d81bbe03530ecb307195b3a44bc
zanderbest <zanderbest@gmail.com>
2013-08-27 00:18:42 -0500

    add exercise 30 init commit

---
+++ ex30.py
@@ -0,0 +1,16 @@
+#Exercise 30 of Learn Python the Hardway
+#Exercise done by Zander Best
+#Learn Python the Hard way is Copyright (C) 2010 Zed. A. Shaw
+#
+#I'm concurrently learning Git along with Zed's Guide. 
+
+people = 20
+cars = 30
+buses = 15
+
+if cars > people:
+	print "We should take the cars"
+elif cars < people:
+	print "We should not take the cars"
+else:
+	print "we cant make up our minds."
