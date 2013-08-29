53060769b4f6184d105c909eeeae10c333ca614d
zanderbest <zanderbest@gmail.com>
2013-08-29 01:11:28 -0500

    init commit playing with dictionaries

---
+++ ex40-2.py
@@ -0,0 +1,11 @@
+#Exercise 40 of Learn Python the Hardway
+#Exercise done by Zander Best
+#Learn Python the Hard way is Copyright (C) 2010 Zed. A. Shaw
+#
+#I'm concurrently learning Git along with Zed's Guide. 
+
+import states
+
+__getitem__(states)
+
+print states['Kansas']
