3ef5c105c42295aba7919521af44887efb37cf1d
zanderbest <zanderbest@gmail.com>
2013-08-29 00:51:57 -0500

    init commit

---
+++ ex40.py
@@ -0,0 +1,6 @@
+#Exercise 40 of Learn Python the Hardway
+#Exercise done by Zander Best
+#Learn Python the Hard way is Copyright (C) 2010 Zed. A. Shaw
+#
+#I'm concurrently learning Git along with Zed's Guide. 
+
